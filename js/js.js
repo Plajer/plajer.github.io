a413aeb0c45edfd5d39cfac2f5d07dd9cf521ac5
Plajer <worthplay.pt@onet.pl>
2018-06-26 17:53:24 +0200

    page update

--- js/js.js
+++ js/js.js
@@ -13,14 +13,14 @@ function animateTitle() {
   function startAnimation() {
     // animate between the original and the new title
 	animatedTitle = titles[Math.floor(Math.random()* titles.length)];
-    document.title = currentState ? origTitle : animatedTitle;
+    document.title = currentState ? "Plajer's Lair" : animatedTitle;
     currentState = !currentState;
   }
 }
 
 function restoreTitle() {
   clearInterval(timer);
-  document.title = origTitle; // restore original title
+  document.title = "Plajer's Lair"; // restore original title
 }
 
 // Change page title on blur
@@ -32,5 +32,38 @@ $(window).blur(function() {
 $(window).focus(function() {
     restoreTitle();
 });
+
+ $('#welcome').hover(
+    function(){
+      var $this = $('#welcome .title');
+      $this.data('transform', $this.css('transform')).css('transform', 'scale(1.05)');
+    },
+    function(){
+      var $this = $('#welcome .title');
+      $this.css('transform', $this.data('transform'));
+    }
+  );
+  
+   $('#footer').hover(
+    function(){
+      var $this = $('#footer .title');
+      $this.data('transform', $this.css('transform')).css('transform', 'scale(1.05)');
+    },
+    function(){
+      var $this = $('#footer .title');
+      $this.css('transform', $this.data('transform'));
+    }
+  );
+ 
+  $('#portfolio').hover(
+    function(){
+      var $this = $('#portfolio .title');
+      $this.data('transform', $this.css('transform')).css('transform', 'scale(1.05)');
+    },
+    function(){
+      var $this = $('#portfolio .title');
+      $this.css('transform', $this.data('transform'));
+    }
+  );  
 
 });
